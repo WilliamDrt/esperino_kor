0bcb411ce8d552f49ce2f9719aaa2959d5464a0c
William <52564695+WilliamDrt@users.noreply.github.com>
2023-11-06 20:30:56 +0200

    add files

--- jscript.js
+++ jscript.js
@@ -12,3 +12,35 @@ function handleScroll() {
     header.classList.remove("sticky");
   }
 }
+
+function todayIs() {
+  const d = new Date();
+  return d.getDay();
+}
+
+function colorToday() {
+  const d = new Date();
+  let day = d.getDay();
+  // alert(day);
+  switch(day){
+    case 1:
+       document.getElementById("day0").style["color"] = "blue";
+      break
+    case 2:
+        document.getElementById("day1").style["color"] = "blue";
+      break
+    case 3:
+       document.getElementById("day2").style["color"] = "blue";
+      break
+    case 4:
+       document.getElementById("day3").style["color"] = "blue";
+    break
+    case 5:
+      document.getElementById("day4").style["color"] = "blue";
+      break 
+    default :
+    alert(Date()); // display current date
+  }
+  
+
+}
