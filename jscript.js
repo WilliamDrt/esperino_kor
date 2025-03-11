7483fc9f6796e427212d58f773b527f5a448be6a
William <52564695+WilliamDrt@users.noreply.github.com>
2025-03-11 19:36:11 +0200

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,47 +1,9 @@
 
 
-// window.onscroll = function() {handleScroll()};
 
-// let header = document.getElementById("myHeader");
-// let sticky = header.offsetTop;
-
-// function handleScroll() {
-//   if (window.scrollY > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
-
-// function todayIs() {
-//   const d = new Date();
-//   return d.getDay();
-// }
 
 function colorToday() {
-  const d = new Date();
-  let day = d.getDay();
-  let dd = "";  
-  // alert(day);
-  switch(day){
-    case 1:
-      dd = "day0";
-      break;
-    case 2:
-      dd = "day1";
-      break;
-    case 3:
-      dd = "day2";
-      break;
-    case 4:
-      dd = "day3";
-    break;
-    case 5:
-      dd = "day4";
-     break;
-  }
-  document.getElementById(dd).style["color"] = "blue";
-  document.getElementById(dd).scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+ 
  
   const hourArr = ["17:00 - 17:40",
                    "17:50 - 18:30",
@@ -127,4 +89,28 @@ for (let iRow = 0; iRow <= 5; iRow++) {
 
 }
 
+ const d = new Date();
+  let day = d.getDay();
+  let dd = "";  
+  // alert(day);
+  switch(day){
+    case 1:
+      dd = "day0";
+      break;
+    case 2:
+      dd = "day1";
+      break;
+    case 3:
+      dd = "day2";
+      break;
+    case 4:
+      dd = "day3";
+    break;
+    case 5:
+      dd = "day4";
+     break;
+  }
+  document.getElementById(dd).style["color"] = "blue";
+  document.getElementById(dd).scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  
 }
